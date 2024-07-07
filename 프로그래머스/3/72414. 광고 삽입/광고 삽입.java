274a5e1dab3112cee488363043704c55c2892443
KimSeMin <kimgiraffe@naver.com>
2024-07-07 16:25:06 +0900

    [level 3] Title: 광고 삽입, Time: 12.76 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class Solution {
+    static int playTime, advTime;
+    static long[] counts;
+    static long max;
+    static int maxTime;
+    
+    static int timeToSeconds(String time) {
+        String[] times = time.split(":");
+        return Integer.parseInt(times[0]) * 3600 + Integer.parseInt(times[1]) * 60 + Integer.parseInt(times[2]);
+    }
+    
+    public String solution(String play_time, String adv_time, String[] logs) {
+        String answer = "";
+        
+        playTime = timeToSeconds(play_time);
+        advTime = timeToSeconds(adv_time);
+        
+        counts = new long[playTime + 1];
+        for(String log : logs) {
+            String[] l = log.split("-");
+            String[] s = l[0].split(":");
+            String[] e = l[1].split(":");
+            
+            int startTime = timeToSeconds(l[0]);
+            int endTime = timeToSeconds(l[1]);
+            counts[startTime]++;
+            counts[endTime]--;
+        }
+        
+        for(int i = 1; i <= playTime; i++) {
+            counts[i] += counts[i - 1];
+        }
+        for(int i = 1; i <= playTime; i++) {
+            counts[i] += counts[i - 1];
+        }
+        
+        max = counts[advTime - 1];
+        
+        for(int i = 0; i + advTime <= playTime; i++) {
+            long count = counts[i + advTime] - counts[i];
+            if(count <= max) {
+                continue;
+            }
+            max = count;
+            maxTime = i + 1;
+        }
+        
+        int h = maxTime / 3600;
+        int m = (maxTime % 3600) / 60;
+        int s = maxTime % 60;
+        
+        answer = String.format("%02d:%02d:%02d", h, m, s);
+        
+        return answer;
+    }
+}
