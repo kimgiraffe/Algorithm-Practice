18bbeec68dc954d1edaf760e161c771a56b526be
KimSeMin <kimgiraffe@naver.com>
2024-07-23 12:49:00 +0900

    Time: 2 ms (96.21%), Space: 45.2 MB (33.37%) - LeetHub

--- 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.java
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.java
@@ -14,48 +14,39 @@
  * }
  */
 class Solution {
-    int answer;
-
-    List<Integer> dfs(TreeNode root, int distance) {
-        if (root == null) {
-            return new ArrayList<Integer>();
+    int answer = 0;
+    final int MAX_DISTANCE = 10;
+    public int[] postOrder(TreeNode node, int distance) {
+        if (node == null) {
+            return new int[MAX_DISTANCE + 1]; // 빈 배열 반환
         }
 
-        if (root.left == null && root.right == null) { // leaf node
-            List<Integer> subList = new ArrayList<Integer>();
-            subList.add(1);
-            return subList;
+        if (node.left == null && node.right == null) { // 리프 노드인 경우...
+            int[] result = new int[MAX_DISTANCE + 1];
+            result[1] = 1; // 부모 노드로부터의 거리는 1
+            return result;
         }
 
-        // 부모 노드로부터 거리 구하기
-        List<Integer> list1 = dfs(root.left, distance); // 왼쪽 subtree 순회
-        List<Integer> list2 = dfs(root.right, distance); // 오른쪽 subtree 순회
+        int[] leftDistance = postOrder(node.left, distance);
+        int[] rightDistance = postOrder(node.right, distance);
 
-        // 부모 노드로부터의 거리 합이 두 leaf 노드 사이의 최단거리
-        for (int d1 : list1) {
-            for (int d2 : list2) {
-                if (d1 + d2 <= distance) { // 조건을 만족하는 경우...
-                    answer++;
-                }
+        for (int i = 1; i <= distance; i++) {
+            for (int j = 1; j <= distance - i; j++) {
+                answer += leftDistance[i] * rightDistance[j];
             }
         }
 
-        // 다음 부모 노드를 위해 거리 1 증가
-        List<Integer> list = new ArrayList<>();
-
-        for (int d : list1) {
-            list.add(d + 1);
+        int[] result = new int[MAX_DISTANCE + 1];
+        // left와 right에 대해 합치기
+        for (int i = 1; i < MAX_DISTANCE; i++) {
+            result[i + 1] = leftDistance[i] + rightDistance[i];
         }
 
-        for (int d : list2) {
-            list.add(d + 1);
-        }
-
-        return list;
+        return result;
     }
 
     public int countPairs(TreeNode root, int distance) {
-        dfs(root, distance);
+        postOrder(root, distance);
 
         return answer;
     }
