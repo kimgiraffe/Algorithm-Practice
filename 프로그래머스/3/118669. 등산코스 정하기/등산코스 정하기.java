ec10f045e3b65cfbb35aeb4256344cd7e845d8fe
KimSeMin <kimgiraffe@naver.com>
2024-07-30 12:59:04 +0900

    [level 3] Title: 등산코스 정하기, Time: 2424.98 ms, Memory: 163 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
@@ -23,34 +23,38 @@ class Solution {
         
         Queue<Node> queue = new ArrayDeque<>();
         
+        // 출입구를 큐에 넣기
         for(int gate : gates) {
             queue.offer(new Node(gate, 0));
-            intensity[gate] = 0;
+            intensity[gate] = 0; // 시작 지점의 intensity는 0
         }
         
         while(!queue.isEmpty()) {
             Node cur = queue.poll();
             
+            // 현재 지점의 소요시간이 기존 가중치보다 큰 경우... 건너뜀
+            if(cur.weight > intensity[cur.vertex]) continue;
+            
             for(int i = 0; i < adjList.get(cur.vertex).size(); i++) {
                 Node tmp = adjList.get(cur.vertex).get(i);
                 
                 int distance = Math.max(intensity[cur.vertex], tmp.weight);
-                if(intensity[tmp.vertex] > distance) {
+                if(intensity[tmp.vertex] > distance) { // intensity 갱신
                     intensity[tmp.vertex] = distance;
                     queue.offer(new Node(tmp.vertex, distance));
                 }
             }
         }
         
-        int summitIdx = Integer.MAX_VALUE;
-        int minIntensity = Integer.MAX_VALUE;
+        int summitIdx = Integer.MAX_VALUE; // 산봉우리 번호
+        int minIntensity = Integer.MAX_VALUE; // intensity의 최솟값
         
-        Arrays.sort(summits);
+        Arrays.sort(summits); // 산봉우리 번호 오름차순 정렬
         
         for(int summit : summits) {
             if(intensity[summit] < minIntensity) {
-                summitIdx = summit;
-                minIntensity = intensity[summit];
+                summitIdx = summit; // 산봉우리 번호 갱신
+                minIntensity = intensity[summit]; // intensity 최솟값 갱신
             }
         }
         
@@ -88,6 +92,9 @@ class Solution {
             int start = path[0];
             int end = path[1];
             int weight = path[2];
+            
+            // 출입구에 연결된 양방향 등산로 -> 출입구에서 다른 지점으로만 이동 가능한 단방향 등산로
+            // 산봉우리에 연결된 양방향 등산로 -> 다른 지점에서 산봉우리로만 이동 가능한 단방향 등산로
             
             if(isGate(start, gates) || isSummit(end, summits)) {
                 adjList.get(start).add(new Node(end, weight));
