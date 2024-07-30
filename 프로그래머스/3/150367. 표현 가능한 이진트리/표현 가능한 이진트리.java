cf403b3fbc5b96b91e76404d9804ac182cc52642
KimSeMin <kimgiraffe@naver.com>
2024-07-30 20:47:28 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 38.82 ms, Memory: 95.6 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -1,10 +1,12 @@
 class Solution {
-    String getBinaryTree(long number) {
-        String binary = Long.toBinaryString(number); // 이진수 변환
-        int height = (int) Math.ceil(Math.log10(binary.length() + 1) / Math.log10(2)); // 이진트리의 높이
+    public String makeBinary(long number) {
+        String binary = Long.toBinaryString(number);
+        
+        int height = (int) Math.ceil(Math.log10(binary.length() + 1) / Math.log10(2));
         int size = (int) Math.pow(2, height) - 1;
+        
         StringBuilder sb = new StringBuilder();
-        // 포화이진트리 생성
+        
         for(int i = 0; i < size - binary.length(); i++) {
             sb.append("0");
         }
@@ -12,19 +14,19 @@ class Solution {
         return sb.toString();
     }
     
-    boolean check(String binaryTree) {
+    public boolean check(String binaryTree) {
         if(binaryTree.length() <= 1) {
             return true;
         }
         
         String leftTree = binaryTree.substring(0, binaryTree.length() / 2);
-        String rightTree = binaryTree.substring(binaryTree.length() /2 + 1);
+        String rightTree = binaryTree.substring(binaryTree.length() / 2 + 1);
         
         char root = binaryTree.charAt(binaryTree.length() / 2);
         char left = leftTree.charAt(leftTree.length() / 2);
         char right = rightTree.charAt(rightTree.length() / 2);
         
-        if(root == '0' && (left == '1' || right == '1')) { // 더미 노드
+        if(root == '0' && (left == '1' || right == '1')) {
             return false;
         } else {
             return check(leftTree) && check(rightTree);
@@ -35,7 +37,7 @@ class Solution {
         int[] answer = new int[numbers.length];
         
         for(int idx = 0; idx < numbers.length; idx++) {
-            if(check(getBinaryTree(numbers[idx]))) {
+            if(check(makeBinary(numbers[idx]))) {
                 answer[idx] = 1;
             } else {
                 answer[idx] = 0;
