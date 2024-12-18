c33ff8b1f710cb46adbae67e9a3e36be18708763
KimSeMin <kimgiraffe@naver.com>
2024-12-19 07:51:51 +0900

    Time: 1 ms (98.94%), Space: 43.7 MB (49.15%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        int[] finalPrice = prices.clone();
+
+        for (int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(prices[j] <= prices[i]) {
+                    finalPrice[i] = prices[i] - prices[j];
+                    break;
+                }
+            }
+        }
+
+        return finalPrice;
+    }
+}
