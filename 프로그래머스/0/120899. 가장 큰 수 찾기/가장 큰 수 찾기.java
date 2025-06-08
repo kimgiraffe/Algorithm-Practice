e25ca19d11b03dbe325eea26200c6d4e4b5927c0
KimSeMin <kimgiraffe@naver.com>
2025-06-08 18:56:30 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        int max = 0;
+        
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                answer[1] = i;
+                answer[0] = max;
+            }
+        }
+        
+        return answer;
+    }
+}
